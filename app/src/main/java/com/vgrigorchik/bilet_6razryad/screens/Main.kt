97e674f086f5dda68fcf458e928d199a0c1bcbb6
vgrigorchik <vgrigorchik@gmail.ru>
2023-09-29 10:03:31 +0300

    сделал экраны и навигацию

---
+++ app/src/main/java/com/vgrigorchik/bilet_6razryad/screens/Main.kt
@@ -0,0 +1,8 @@
+package com.vgrigorchik.bilet_6razryad.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun Main(navController: NavController) {
+}
